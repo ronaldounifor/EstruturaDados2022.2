d516bd15f018249db6efd104cef424895a815855
PROFESSOR D18 <professord18@acad.unifor.br>
2022-08-18 10:51:51 -0300

    Criando a estrutura Pilha (não implementado)

---
+++ Estruturas/src/estruturas/Pilha.java
@@ -0,0 +1,28 @@
+package estruturas;
+
+public class Pilha {
+    int[] elementos;
+    int quantidade;
+
+    //Criar uma nova pilha – Pilha()
+    Pilha() {}
+    Pilha(int limite) {}
+
+    //Retornar a quantidade de elementos – getQuantidade()
+    int getQuantidade() { return -1;}
+    
+    //Verificar se a pilha está vazia – estaVazia()
+    //Verificar se a pilha está cheia, caso exista um limite – estaCheia()
+
+    //Inserir um elemento e no topo da pilha – empilhar(e)
+    void empilhar(int elemento) {}
+
+    //Remover o elemento e no topo da pilha – desempilhar()
+    int desempilhar() {return -1;}
+
+    //Acessar o elemento no topo da pilha – retornarElemento()
+    int retornarElemento() {return -1;}
+
+
+    //exibir() {}
+}

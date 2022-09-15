b569cf927fb1cf3e6325a9671327292dbfceaf88
PROFESSOR D18 <professord18@acad.unifor.br>
2022-09-15 10:59:39 -0300

    Implementação do adicionarPosicao, validacao de entrada, estaVazia e getQuantidade

--- Estruturas/src/estruturas/dinamica/LSE.java
+++ Estruturas/src/estruturas/dinamica/LSE.java
@@ -1,5 +1,7 @@
 package estruturas.dinamica;
 
+import util.Validador;
+
 /**
  * Lista Simplesmente Encadeada (LSE)
  */
@@ -16,24 +18,55 @@ public class LSE {
     }
 
     //Retornar a quantidade de elementos – getQuantidade()
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
     //Verificar se a lista está vazia – estaVazia()
+    public boolean estaVazia() {
+        if(quantidade == 0)
+            return true;
+        else
+            return false;
+    }
+
     //Acessar o elemento na posição i – retornarElemento(i)
 
 
+    //Checa se a posicao desejada é valida, ou seja, está dentro do vetor de elementos
+    private boolean entradaValida(int posicao) {
+        if(posicao >= 0 && posicao <= quantidade)
+            return true;
+        else
+            return false;
+    }
+
     //Inserir um elemento e na posição i – adicionarPosicao(e, i)
     public void adicionarPosicao(No noNovo, int posicao) {
-        //achar antigoNo
-        No noAntigo = primeiro;
-        No noAnterior;
-        for(int i = 0; i < posicao; i++) {
-            noAnterior = noAntigo;
-            noAntigo = noAntigo.getProximo();
-        }
-        //novoNo apontar pro antigoNo
-        noNovo.setProximo(noAntigo);
-        //anterior apontar pro novoNo
+        if(!entradaValida(posicao)) {
+            System.out.println("Posição deve ser maior ou igual a zero e menor ou igual a quantidade.");
+        } else {
+            No noAntigo = primeiro;
+            No noAnterior = null;
+            for(int i = 0; i < posicao; i++) {
+                noAnterior = noAntigo;
+                noAntigo = noAntigo.getProximo();
+            }
+            
+            noNovo.setProximo(noAntigo);
 
-        //aumentar a quantidade
+            //Se o nó na antiga posição (noAntigo) for nulo, então o novo nó (noNovo) está sendo adicionado na última posição.
+            if(noAntigo == null)
+                this.ultimo = noNovo;
+            
+            //Se o nó anterior (noAnterior) for nulo, então o novo nó (noNovo) está sendo adicionado na primeira posição. 
+            if(noAnterior == null)
+                this.primeiro = noNovo;
+            else
+                noAnterior.setProximo(noNovo);
+                
+            this.quantidade++;
+        }
     }
 
     //Inserir um elemento e no início – adicionarInicio(e)
